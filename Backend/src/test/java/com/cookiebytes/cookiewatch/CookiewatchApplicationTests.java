e7e2c7b3f747f6245e1864d15800c780e3f2e600
G5andeepD <e19063@eng.pdn.ac.lk>
2024-04-20 16:26:15 +0530

    second in initial commit

---
+++ Backend/src/test/java/com/cookiebytes/cookiewatch/CookiewatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookiebytes.cookiewatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiewatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
