ff45306cc27a463e04b0ca55dadb8e7ca1d37732
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-04-20 20:17:44 +0530

    Pages in react

---
+++ ProfessionalDashboard/src/page_gen/index.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='search'>
+        <div className='search-1'>
+          <div className='vector' />
+        </div>
+        <span className='search-text'>Search</span>
+      </div>
+      <div className='flex-row-af'>
+        <div className='ellipse' />
+        <div className='ellipse-2' />
+        <div className='ellipse-3' />
+        <div className='peak-user-settings'>
+          <div className='header'>
+            <div className='header-4'>
+              <div className='icon-button'>
+                <div className='button'>
+                  <div className='sun' />
+                </div>
+                <div className='button-5'>
+                  <div className='bell' />
+                </div>
+                <div className='profile-image' />
+              </div>
+              <div className='icon-breadcrumb'>
+                <div className='breadcrumb'>
+                  <div className='button-6'>
+                    <span className='text' />
+                  </div>
+                  <div className='button-7'>
+                    <span className='text-8'>Generate Medical Records for</span>
+                  </div>
+                </div>
+              </div>
+              <div className='search-9'>
+                <input className='search-input' />
+                <div className='search-a'>
+                  <div className='vector-b' />
+                </div>
+                <span className='text-c'>Search by NIC or Email</span>
+              </div>
+              <div className='save-button' />
+              <div className='search-d'>
+                <div className='vector-e' />
+              </div>
+            </div>
+          </div>
+          <div className='flex-row-f'>
+            <div className='general-info-photo'>
+              <div className='group'>
+                <div className='group-f'>
+                  <div className='ellipse-10' />
+                </div>
+                <div className='flex-column-f'>
+                  <span className='miss-alaa-mohamed'>Miss. Alaa Mohamed</span>
+                  <span className='username'>username : </span>
+                  <span className='nic'>NIC : </span>
+                </div>
+              </div>
+            </div>
+            <div className='frame' />
+          </div>
+          <div className='flex-row-e'>
+            <div className='activated-item' />
+            <div className='regroup'>
+              <div className='name'>
+                <span className='blood-type'>Blood Type</span>
+                <div className='name-field' />
+              </div>
+              <div className='name-11'>
+                <span className='height'>Height</span>
+                <div className='name-field-12' />
+              </div>
+              <div className='name-13'>
+                <span className='weight'>Weight</span>
+                <div className='name-field-14' />
+              </div>
+            </div>
+            <div className='name-15'>
+              <span className='age'>Age</span>
+              <div className='name-field-16' />
+            </div>
+          </div>
+          <div className='email-phone'>
+            <div className='flex-row-f-17'>
+              <span className='email-address'>Email Address</span>
+              <span className='phone-number'>Phone Number</span>
+            </div>
+            <div className='flex-row-fd'>
+              <div className='email-field' />
+              <div className='phone-field' />
+            </div>
+          </div>
+          <div className='location'>
+            <span className='residential-location'>Residential Location</span>
+            <div className='location-field' />
+          </div>
+          <div className='vector-18' />
+          <div className='flex-row-ea'>
+            <div className='name-19'>
+              <span className='diagnosis'>Diagnosis</span>
+              <div className='name-field-1a' />
+              <span className='eg-alaa-mohamed'>eg. alaa.mohamed</span>
+            </div>
+            <div className='name-1b'>
+              <span className='medications'>Medications</span>
+              <div className='name-field-1c' />
+              <span className='eg-alaa-mohamed-1d'>eg. alaa.mohamed</span>
+            </div>
+          </div>
+          <div className='name-1e'>
+            <span className='comments'>Comments</span>
+            <div className='name-field-1f' />
+            <span className='eg-alaa-mohamed-20'>eg. alaa.mohamed</span>
+          </div>
+          <div className='flex-row-fbe'>
+            <button className='cancel-button'>
+              <span className='cancel'>Cancel</span>
+            </button>
+            <button className='save-button-21'>
+              <span className='text-22'>Save Changes</span>
+            </button>
+          </div>
+          <div className='vector-23' />
+        </div>
+        <div className='rectangle'>
+          <span className='medical-report'>Medical Report of</span>
+        </div>
+        <div className='search-24'>
+          <input className='search-input-25' />
+          <div className='search-26'>
+            <div className='vector-27' />
+          </div>
+          <span className='text-28'>Search</span>
+        </div>
+        <span className='your-profile'>Your Profile</span>
+        <span className='generate-medical-records'>
+          Generate Medical Records
+        </span>
+        <div className='frame-29'>
+          <span className='view-medical-records'>View Medical Records</span>
+        </div>
+        <div className='frame-2a'>
+          <span className='hospital-admission'>Hospital Admission</span>
+        </div>
+        <div className='rectangle-2b'>
+          <span className='sign-out'>Sign out</span>
+        </div>
+      </div>
+    </div>
+  );
+}
