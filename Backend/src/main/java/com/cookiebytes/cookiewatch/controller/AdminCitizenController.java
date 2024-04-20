05c9b42ab9e6e8d5dc24456906dad197b2086f46
G5andeepD <e19063@eng.pdn.ac.lk>
2024-04-20 18:09:59 +0530

    swagger cors and small error

--- Backend/src/main/java/com/cookiebytes/cookiewatch/controller/AdminCitizenController.java
+++ Backend/src/main/java/com/cookiebytes/cookiewatch/controller/AdminCitizenController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("api/v1/admin/citizen")
 public class AdminCitizenController {
     private final AdminCitizenService adminCitizenService;
