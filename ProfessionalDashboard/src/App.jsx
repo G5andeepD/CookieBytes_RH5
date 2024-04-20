46c0c1635a583feebc4a745d0f6c1466884ae957
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-04-21 02:58:02 +0530

    token

--- ProfessionalDashboard/src/App.jsx
+++ ProfessionalDashboard/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import UserProfile from './USERPROFILE/userprofile';
 import GenerateMedicals from './USERPROFILE/generatemedical';
+import SignIN from './USERPROFILE/signin';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path='/userprofile' element={<UserProfile />} />
         <Route path='/generatemedicals' element={<GenerateMedicals />} />
+        <Route path='/' element={<SignIN />} />
       </Routes>
     </Router>
   );
