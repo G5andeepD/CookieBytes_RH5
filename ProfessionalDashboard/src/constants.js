d7bb6a93896529f0ca1d11df5b80c093fa384baf
Buddhi19 <wijenayakebuddhi34802@gmail.com>
2024-04-21 05:30:49 +0530

    fend

--- ProfessionalDashboard/src/constants.js
+++ ProfessionalDashboard/src/constants.js
@@ -1,2 +1,2 @@
-export const LOCAL_SERVER_URL = "http://192.168.1.11:5000/api/v1/";
-export const LOCAL_HOST_URL = "http://localhost:5000/api/v1/"; 
+export const LOCAL_HOST_URL = "http://192.168.1.11:5000/api/v1/";
+export const LOCAL_SERVER_URL = "http://localhost:5000/api/v1/"; 
